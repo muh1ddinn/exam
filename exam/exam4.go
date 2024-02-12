89416e05020c09d770cb37a1c3c508ad6cbc85b9
muh1ddinn <rmamuhit@gmail.com>
2024-02-12 19:28:42 +0500

    exam

--- exam/exam4.go
+++ exam/exam4.go
@@ -1,34 +1,33 @@
-package exam
+package main
 
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
 )
 
 func main() {
-
 	filename := ""
 	fmt.Scan(&filename)
 
-	if checkfilee(filename + ".txt") {
-
-		var n = 0
-
-		for i:= 0;i<n;i++{
-			a:=fmt.Sprint(i)
-        var rec1+"a" = filename
-		rec1+i 
-	
+	n := 3
+	for i := 0; i < n; i++ {
+		newFilename := filename + strconv.Itoa(rand.Intn(50))
+		if !checkFileExists(newFilename + ".txt") {
+			_, err := os.Create(newFilename + ".txt")
+			if err != nil {
+				fmt.Println("Error creating file:", err)
+				return
+			}
+			fmt.Println("Created file:", newFilename)
+			break
+		}
 	}
-
-	}
-
 }
 
-func checkfilee(filename string) bool {
-
+func checkFileExists(filename string) bool {
 	_, err := os.Stat(filename)
-
-	return !errors.Is(err, os.ErrNoExist)
-
+	return !errors.Is(err, os.ErrNotExist)
 }
